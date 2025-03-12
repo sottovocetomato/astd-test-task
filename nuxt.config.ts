4ab33e8a336d44630ac09b354c470eb8703143ca
ai-strings <adrstrings@gmail.com>
2025-03-12 12:57:27 +0300

    caching test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
-import { defineNuxtConfig } from 'nuxt/config'
-import tailwindcss from '@tailwindcss/vite'
+import { defineNuxtConfig } from "nuxt/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   modules: [
-    '@pinia/nuxt',
-    '@nuxt/icon',
-    '@nuxt/eslint',
-    '@nuxtjs/google-fonts',
+    "@pinia/nuxt",
+    "@nuxt/icon",
+    "@nuxt/eslint",
+    "@nuxtjs/google-fonts",
   ],
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  compatibilityDate: '2024-11-01',
+  css: ["~/assets/css/main.css"],
+  compatibilityDate: "2024-11-01",
   vite: {
     plugins: [tailwindcss()],
   },
@@ -19,12 +19,28 @@ export default defineNuxtConfig({
       stylistic: true,
     },
   },
+  routeRules: {
+    "/": { prerender: true },
+    // "/catalog": { swr: true },
+    // "/catalog/**": { swr: 3600 },
+    // "/products/**": { swr: 3600 },
+  },
   googleFonts: {
-    display: 'swap',
+    display: "swap",
     families: {
-      'Roboto': [400, 500, 600, 700],
-      'Inter': [300, 400, 500, 600, 700, 800, 900],
-      'Golos Text': [400, 500, 600, 700, 800, 900],
+      Roboto: [400, 500, 600, 700],
+      Inter: [300, 400, 500, 600, 700, 800, 900],
+      "Golos Text": [400, 500, 600, 700, 800, 900],
     },
   },
-})
+  $development: {
+    nitro: {
+      storage: {
+        catalog: {
+          driver: "fs",
+          base: "catalogs",
+        },
+      },
+    },
+  },
+});
--- server/api/catalog/[slug].ts
+++ server/api/catalog/[slug].ts
@@ -1,13 +1,21 @@
 import { generateFakeProduct } from "~/utils/generate";
 import type { Product } from "~/utils/generate";
 
-export default defineEventHandler((event) => {
-  // const name = getRouterParam(event, 'slug')
-  const products: Product[] = [];
-  while (products.length < 12) {
-    products.push(generateFakeProduct());
+export default defineCachedEventHandler(
+  (event) => {
+    // const name = getRouterParam(event, 'slug')
+    const products: Product[] = [];
+    while (products.length < 12) {
+      products.push(generateFakeProduct());
+    }
+    return {
+      products,
+    };
+  },
+  {
+    base: "catalogs",
+    getKey: () => "recepies",
+    maxAge: 3600,
+    staleMaxAge: 3600,
   }
-  return {
-    products,
-  };
-});
+);
